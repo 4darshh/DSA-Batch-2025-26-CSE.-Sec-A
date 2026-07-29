0fddd38cc248c4fe2f4e0c5206331891c9902e01
Arnik Bose <arnikbose3@gmail.com>
2026-07-29 23:16:40 +0530

    Add files via upload

---
+++ 28-07-2026/32/reverse in array.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int arr[] = {10, 20, 30, 40, 50};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original array: ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    int start = 0;
+    int end = size - 1;
+
+    while (start < end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+
+        start++;
+        end--;
+    }
+
+    printf("Reversed array: ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
