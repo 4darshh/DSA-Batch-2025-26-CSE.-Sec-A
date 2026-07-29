985d94c4f536792134f21c339e7573d922ccfc40
deyarpan4484-lang <deyarpan4484@gmail.com>
2026-07-29 23:20:08 +0530

    Add files via upload

---
+++ 28-07-2026/31/LAB ASSIGNMENT 1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, i;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter the array elements:\n");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Delete the first element
+    for(i = 0; i < n - 1; i++) {
+        arr[i] = arr[i + 1];
+    }
+
+    n--; // Reduce array size
+
+    printf("Array after deleting the first element:\n");
+    for(i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
