54b4a0fe138535da2ce5b0635d40636b2ae41afb
Mohan -07 <mohanmaji2004@gmail.com>
2026-08-01 21:52:17 +0530

    Add files via upload

---
+++ 28-07-2026/58/lenear search of an array.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int linear_search(int arr[],int size,int target){
+    for(int i=0;i<size;i++){
+        if(arr[i]==target){
+            return i;
+        }
+    }return -1;
+}
+int main(){
+    int target,result;
+    int arr[]={3,45,74,52,65,15};
+    int size=sizeof(arr)/sizeof(arr[0]);
+    printf("enter the target value :");
+    scanf("%d",&target);
+    result=linear_search(arr,size,target);
+    if(result!=-1){
+        printf("element found at index :%d\n",result);
+     }
+     else{
+         printf("element not found at index :%d\n",result);
+     }
+     return 0;
+}
