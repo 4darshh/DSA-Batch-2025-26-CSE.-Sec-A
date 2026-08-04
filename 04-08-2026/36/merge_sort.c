54a790d6a65f7c58f91d3cdbf0d12a6f6c153a83
Debayan Pandey <debayanpandey11@gmail.com>
2026-08-04 14:05:16 +0530

    Implement merge sort algorithm in C

--- 04-08-2026/36/merge_sort.c
+++ 04-08-2026/36/merge_sort.c
@@ -1,33 +1,88 @@
-#include<stdio.h>
-int main(){
+#include <stdio.h>
+
+void merge(int arr[], int left, int mid, int right)
+{
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    int L[n1], R[n2];
+
+    for(int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+
+    for(int j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
+
+    int i = 0, j = 0, k = left;
+
+    while(i < n1 && j < n2)
+    {
+        if(L[i] <= R[j])
+        {
+            arr[k] = L[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while(i < n1)
+    {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while(j < n2)
+    {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(int arr[], int left, int right)
+{
+    if(left < right)
+    {
+        int mid = (left + right) / 2;
+
+        mergeSort(arr, left, mid);
+
+        mergeSort(arr, mid + 1, right);
+
+        merge(arr, left, mid, right);
+    }
+}
+
+int main()
+{
     int n;
-    printf(" Enter The Array Size : ");
+
+    printf("Enter The Array Size : ");
     scanf("%d", &n);
+
     int arr[n];
-    printf(" Enter The Array : ");
-    for(int i=0;i<n;i++){
+
+    printf("Enter The Array : ");
+
+    for(int i = 0; i < n; i++)
+    {
         scanf("%d", &arr[i]);
     }
-    for(int j=0;j<n/2;j++){
-        for(int k=j+1;k<n;k++){
-            if(arr[j]>arr[k]){
-                int sum = arr[k];
-                arr[k]  = arr[j];
-                arr[j] = sum;
-            }
-        }
+
+    mergeSort(arr, 0, n - 1);
+
+    printf("Sorted Array : ");
+
+    for(int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
     }
-       for(int j=n/2;j<n;j++){
-        for(int k=j+1;k<n;k++){
-            if(arr[j]>arr[k]){
-                int sum = arr[k];
-                arr[k]  = arr[j];
-                arr[j] = sum;
-            }
-        }
-    }
-    printf(" Enter The Sorted Array  : ");
-    for(int i=0;i<n;i++){
-        printf("%d ",arr[i]);
-    }
+
+    return 0;
 }
