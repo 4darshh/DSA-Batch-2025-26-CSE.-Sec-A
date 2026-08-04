e853d9bb148592beed6e4242f53264ed11d7ed25
Bedanta Chakraborty <bedantachakraborty2000555@gmail.com>
2026-08-04 19:42:28 +0530

    Add files via upload

---
+++ 04-08-2026/54/Iterative_Quick_Sort.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+ 
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int stack[n]; 
+    int top = -1;
+
+   
+    stack[++top] = 0;
+    stack[++top] = n - 1;
+
+    while (top >= 0) {
+      
+        int high = stack[top--];
+        int low = stack[top--];
+
+      
+        int pivot = arr[high];
+        int i = (low - 1);
+
+        for (int j = low; j <= high - 1; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                // Swap arr[i] and arr[j]
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        int p = i + 1;
+
+       
+        if (p - 1 > low) {
+            stack[++top] = low;
+            stack[++top] = p - 1;
+        }
+
+       
+        if (p + 1 < high) {
+            stack[++top] = p + 1;
+            stack[++top] = high;
+        }
+    }
+
+  
+    printf("Sorted array (Quick Sort):\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    
+    return 0;
+}
